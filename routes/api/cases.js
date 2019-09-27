7457d9a251100ec80413edc2f91230f8a0eefabb
Damir Medakovic <damir0096@hotmail.com>
2019-09-27 22:43:54 +0200

    created client side react app

--- routes/api/cases.js
+++ routes/api/cases.js
@@ -5,12 +5,29 @@ const router = express.Router();
 
 const Case = require('../../models/case');
 
-
+//Get all cases
 router.get('/', (req, res) => {
 
 	Case.find().sort({ date: -1 }).then(cases => res.json(cases));
 });
 
+//Post a case
+router.post('/', (req, res) => {
+
+	const newCase = new Case({
+		name: req.body.name
+	});
+
+	newCase.save().then(item => res.json(item));
+});
+
+//Delete a case
+router.delete ('/:id', (req, res) => {
+
+	Case.findById(req.params.id)
+	.then(entry => entry.remove().then(() => res.json({success: true})))
+	.catch(err => res.status(404).json({failure: `${err}`}));
+});
 
 module.exports = router; 
 
